de6df0de5ff27bdf937c7673d883566a21fdfddc
Naresh Kumar Bulusu <naresh.bulusu@gmail.com>
2021-01-11 16:01:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 
-resource "azurerm_storage_account" "example" {
-  name                     = "storageaccountname"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+resource "azurerm_storage_account" "main" {
+  name                     = var.storageaccountname
+  resource_group_name      = var.resource_group_name
+  location                 = var.location
+  account_tier             = var.account_tier
+  account_replication_type = var.account_replication_type
 
-  tags = {
-    environment = "staging"
-  }
+  tags =                   = var.tags
 }
