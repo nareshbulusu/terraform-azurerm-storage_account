a34f91c804795128a46aac8365f2b1dbe6d7626d
Naresh Kumar Bulusu <naresh.bulusu@gmail.com>
2021-01-11 16:07:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,5 @@ resource "azurerm_storage_account" "main" {
   location                 = var.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
-
-  tags =                   = var.tags
+  tags                     = var.tags
 }
